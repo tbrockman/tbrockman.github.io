30d9eaa56579c9723e045788b5a84c5f2e2ab8d5
tbrockman <tbrockma@ualberta.ca>
2017-07-11 01:39:46 -0600

    printer friendly resume and resume changes

--- update.js
+++ update.js
@@ -9,7 +9,7 @@ function query_age() {
 
 function update_age_text(age) {
     var text = "I'm a " + age +
-    "-year-old software developer who spends most of his time playing video games and rock climbing. \
+    "-year-old software developer who spends most of his spare time playing video games and rock climbing. \
     Sometimes I try to be creative through music and other mediums. \
     I like coffee and hip hop and have never taken a long walk on the beach.";
 
@@ -19,7 +19,7 @@ function update_age_text(age) {
 }
 
 function update_copyright_text() {
-    var text = "copyright © " + (CURR.getYear() + 1900) + " thedoore brockman";
+    var text = "copyright © " + (CURR.getYear() + 1900) + " theodore brockman";
     var ele = document.getElementById("copyright");
     ele.innerHTML = text;
     return text;
