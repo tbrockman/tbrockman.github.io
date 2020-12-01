514865cb679da9a8833313d896fd6d8b4a8114c7
Theodore Brockman <tmanbrock@gmail.com>
2020-11-30 21:28:08 -0800

    Finalizing stylings for resume.

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -3,6 +3,7 @@ const dashTemplate = (a) => {
 }
 
 const init = () => {
+
     const resumeContainer = document.getElementsByClassName('resume-container')[0]
 
     let dashes = "-"
@@ -11,13 +12,24 @@ const init = () => {
 
     const elements = document.querySelectorAll(".section-separator");
     elements[0].innerHTML = string
+    const lengthBefore = elements[0].clientWidth
     console.log(elements[0].clientWidth)
     console.log(elements[0].innerHTML)
+    console.log(width)
 
-    while (elements[0].clientWidth < width - 50) {
+    dashes += "-"
+    elements[0].innerHTML = dashTemplate(dashes)
+    const lengthAfter = elements[0].clientWidth 
+
+    const delta = lengthAfter - lengthBefore
+    let fill = Math.floor((width - lengthAfter) / delta) 
+    console.log(lengthBefore, lengthAfter, delta, fill)
+
+    while (fill > 0) {
         dashes += "-"
-        elements[0].innerHTML = dashTemplate(dashes)
+        fill--    
     }
+    elements[0].innerHTML = dashTemplate(dashes)
 
     elements.forEach(element => {
         element.innerHTML = dashTemplate(dashes)
